ad8dc2ed487bd0f9f88eef0020e3092e76f87f5b
Mhyssa <jolym8@gmail.com>
2024-05-30 15:15:43 +0200

    :sparkles: Alter controller with Repository

--- src/Controller/AnnuaireController.php
+++ src/Controller/AnnuaireController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,28 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AnnuaireController extends AbstractController
 {
     #[Route('/', name: 'app_annuaire')]
-    public function index(): Response
+    public function index(ContactRepository $contactRepository): Response
     {
-        $contacts = [
-            $this->createContact('Marple','Tania','0123456789'),
-            $this->createContact('George','Saw','0123456789'),
-            $this->createContact('Dawson','Jack','0123456789'),
-
-        ];
-
+        $contacts = $contactRepository->findAll();
         return $this->render('annuaire/index.html.twig', [
             'contacts' => $contacts,
-        ]);
+            ]);
     }
 
-    private function createContact(string $nom, string $prenom, string $telephone): Contact
-{
-        $contact = new Contact();
-        $contact->setNom($nom);
-        $contact->setPrenom($prenom);
-        $contact->setTelephone($telephone);
-        return $contact;
-}
+  
+
 }
 
 
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Contact;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $contacts = [
+            $this->createContact('Marple','Tania','0123456789'),
+            $this->createContact('George','Saw','0123456789'),
+            $this->createContact('Dawson','Jack','0123456789'),
+            $this->createContact('Dawson','Rose','0123456789'),
+
+
+        ];
+
+        foreach ($contacts as $contact){
+
+            $manager->persist($contact);
+        }
 
         $manager->flush();
+    }
+    
+    private function createContact(string $nom, string $prenom, string $telephone): Contact
+    {
+            $contact = new Contact();
+            $contact->setNom($nom);
+            $contact->setPrenom($prenom);
+            $contact->setTelephone($telephone);
+            return $contact;
     }
 }
